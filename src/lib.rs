2c190df5c0725ca731fe1a3deaa81cb42ffe1bbe
xiao-e-yun <a0970323059@gmail.com>
2025-09-16 19:58:29 +0800

    docs: add bytes example

--- src/lib.rs
+++ src/lib.rs
@@ -19,6 +19,8 @@
 //! use axum_extra::headers::Range;
 //!
 //! use tokio::fs::File;
+//! use std::io::Cursor;
+//! use bytes::Bytes;
 //!
 //! use axum_range::Ranged;
 //! use axum_range::KnownSize;
@@ -26,6 +28,15 @@
 //! async fn file(range: Option<TypedHeader<Range>>) -> Ranged<KnownSize<File>> {
 //!     let file = File::open("The Sims 1 - The Complete Collection.rar").await.unwrap();
 //!     let body = KnownSize::file(file).await.unwrap();
+//!     let range = range.map(|TypedHeader(range)| range);
+//!     Ranged::new(range, body)
+//! }
+//!
+//! async fn bytes(range: Option<TypedHeader<Range>>) -> Ranged<KnownSize<Cursor<Bytes>>> {
+//!     let buffer = tokio::fs::read("cat.jpg").await.unwrap();
+//!     // processing data here...
+//!     
+//!     let body = KnownSize::bytes(buffer);
 //!     let range = range.map(|TypedHeader(range)| range);
 //!     Ranged::new(range, body)
 //! }
